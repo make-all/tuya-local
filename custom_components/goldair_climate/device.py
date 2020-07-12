d539e5887dc10ee7105d965e9180f57b7df5bce9
Jason Rumney <make-all@users.noreply.github.com>
2020-07-13 11:47:56 +1200

    Avoid switching protocols once communication is working (#37)
    * Be less eager about changing protocol.
    
    Once the device is communicating, only switch protocols after the connection retry limit is reached.
    This prevents compounding the problem, in particular when v3.3 protocol devices send back responses that pytuya does not handle, as switching to v3.1 seems to cause these devices to go offline for a few minutes sometimes.
    
    Co-authored-by: Jason Rumney <jasonrumney@clarion.com.my>

--- custom_components/goldair_climate/device.py
+++ custom_components/goldair_climate/device.py
@@ -37,6 +37,7 @@ class GoldairTuyaDevice(object):
 
         self._name = name
         self._api_protocol_version_index = None
+        self._api_protocol_working = False
         self._api = pytuya.Device(dev_id, address, local_key, "device")
         self._refresh_task = None
         self._rotate_api_protocol_version()
@@ -198,13 +199,15 @@ class GoldairTuyaDevice(object):
         for i in range(self._CONNECTION_ATTEMPTS):
             try:
                 func()
+                self._api_protocol_working = True
                 break
             except Exception as e:
                 _LOGGER.debug(f"Retrying after exception {e}")
                 if i + 1 == self._CONNECTION_ATTEMPTS:
                     self._reset_cached_state()
+                    self._api_protocol_working = False
                     _LOGGER.error(error_message)
-                else:
+                elif self._api_protocol_working is False:
                     self._rotate_api_protocol_version()
 
     def _get_cached_state(self):
