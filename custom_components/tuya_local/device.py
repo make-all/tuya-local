752845f6e6fd8035f7a262a6c3a6694478c8bea8
Jason Rumney <jasonrumney@gmail.com>
2021-06-07 14:28:00 +1200

    Call async_add_executor_job correctly.
    Must call it with pointer to function, and args as separate arguments.
    Was calling it with the result of calling the function inline, which doesn't
    run the fuction asynchronously, and causes an error.

--- custom_components/tuya_local/device.py
+++ custom_components/tuya_local/device.py
@@ -148,7 +148,7 @@ class TuyaLocalDevice(object):
         await self._hass.async_add_executor_job(self.set_property, dps_id, value)
 
     async def async_set_properties(self, dps_map):
-        await self._hass.async_add_executor_job(self._set_properties(dps_map))
+        await self._hass.async_add_executor_job(self._set_properties, dps_map)
 
     def anticipate_property_value(self, dps_id, value):
         """
