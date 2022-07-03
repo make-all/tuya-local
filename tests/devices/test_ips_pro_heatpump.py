c4a8f5095e2e6abf5733e1fe3817b6a3fb0e785d
Christian Garbas <christian.garbas@daimler.com>
2022-07-03 17:14:30 +0200

    fullfill ips-pro model

--- tests/devices/test_ips_pro_heatpump.py
+++ tests/devices/test_ips_pro_heatpump.py
@@ -1,11 +1,8 @@
-from homeassistant.components.climate.const import (
-    ClimateEntityFeature,
-    HVACAction,
-    HVACMode,
-)
+from homeassistant.components.climate.const import HVACMode
 from homeassistant.components.sensor import SensorDeviceClass
 from homeassistant.const import (
     PERCENTAGE,
+    STATE_UNAVAILABLE,
     TEMP_CELSIUS,
     TEMP_FAHRENHEIT,
 )
@@ -16,17 +13,19 @@ from ..mixins.climate import TargetTemperatureTests
 from ..mixins.sensor import BasicSensorTests
 from .base_device_tests import TuyaDeviceTestCase
 
-HVACMODE_DPS = "1"
-CURRENTTEMP_DPS = "102"
+POWER_DPS = "1"
+HVACMODE_DPS = "105"
 UNITS_DPS = "103"
 POWERLEVEL_DPS = "104"
-OPMODE_DPS = "105"
 TEMPERATURE_DPS = "106"
-MINTEMP_DPS = "107"
-MAXTEMP_DPS = "108"
+MIN_TEMPERATUR_DPS = "107"
+MAX_TEMPERATUR_DPS = "108"
 UNKNOWN115_DPS = "115"
 UNKNOWN116_DPS = "116"
 PRESET_DPS = "2"
+# min and max temperature are not static in real model
+MAX_TEMPERATUR = 40
+MIN_TEMPERATUR = 18
 
 
 class TestIpsProHeatpump(
@@ -42,8 +41,8 @@ class TestIpsProHeatpump(
         self.setUpTargetTemperature(
             TEMPERATURE_DPS,
             self.subject,
-            min=18,
-            max=40,
+            MIN_TEMPERATUR,
+            MAX_TEMPERATUR,
         )
         self.setUpBasicSensor(
             POWERLEVEL_DPS,
@@ -54,63 +53,61 @@ class TestIpsProHeatpump(
         )
         self.mark_secondary(["sensor_power_level"])
 
-    def test_supported_features(self):
-        self.assertEqual(
-            self.subject.supported_features,
-            (
-                ClimateEntityFeature.TARGET_TEMPERATURE
-                | ClimateEntityFeature.PRESET_MODE
-            ),
-        )
-
-    def test_icon(self):
-        self.dps[HVACMODE_DPS] = True
-        self.assertEqual(self.subject.icon, "mdi:hot-tub")
-
-        self.dps[HVACMODE_DPS] = False
-        self.assertEqual(self.subject.icon, "mdi:hvac-off")
-
     def test_temperature_unit(self):
         self.dps[UNITS_DPS] = False
         self.assertEqual(self.subject.temperature_unit, TEMP_FAHRENHEIT)
         self.dps[UNITS_DPS] = True
         self.assertEqual(self.subject.temperature_unit, TEMP_CELSIUS)
 
-    def test_minimum_fahrenheit_temperature(self):
-        self.dps[UNITS_DPS] = False
-        self.dps[MINTEMP_DPS] = 60
-        self.assertEqual(self.subject.min_temp, 60)
+    def test_minimum_target_temperature(self):
+        self.dps[TEMPERATURE_DPS] = 18
+        self.assertEqual(self.subject.min_temp, 18)
 
-    def test_maximum_fahrenheit_temperature(self):
-        self.dps[UNITS_DPS] = False
-        self.dps[MAXTEMP_DPS] = 115
-        self.assertEqual(self.subject.max_temp, 115)
-
-    def test_current_temperature(self):
-        self.dps[CURRENTTEMP_DPS] = 25
-        self.assertEqual(self.subject.current_temperature, 25)
+    def test_maximum_target_temperature(self):
+        self.dps[TEMPERATURE_DPS] = 40
+        self.assertEqual(self.subject.max_temp, 40)
 
     def test_hvac_mode(self):
-        self.dps[HVACMODE_DPS] = True
+        self.dps[HVACMODE_DPS] = "warm"
         self.assertEqual(self.subject.hvac_mode, HVACMode.HEAT)
 
-        self.dps[HVACMODE_DPS] = False
+        self.dps[HVACMODE_DPS] = None
+        self.assertEqual(self.subject.hvac_mode, STATE_UNAVAILABLE)
+
+    def test_current_temperature(self):
+        self.assertEqual(self.subject.current_temperature, 10)
+
+    def test_hvac_mode(self):
+        self.dps[POWER_DPS] = True
+        self.dps[HVACMODE_DPS] = "smart"
+        self.assertEqual(self.subject.hvac_mode, HVACMode.HEAT_COOL)
+        self.dps[HVACMODE_DPS] = "cool"
+        self.assertEqual(self.subject.hvac_mode, HVACMode.COOL)
+        self.dps[HVACMODE_DPS] = "warm"
+        self.assertEqual(self.subject.hvac_mode, HVACMode.HEAT)
+        self.dps[POWER_DPS] = False
         self.assertEqual(self.subject.hvac_mode, HVACMode.OFF)
 
-    def test_hvac_modes(self):
-        self.assertCountEqual(self.subject.hvac_modes, [HVACMode.OFF, HVACMode.HEAT])
-
-    async def test_turn_on(self):
+    async def test_set_hvac_mode_to_cool(self):
         async with assert_device_properties_set(
-            self.subject._device, {HVACMODE_DPS: True}
+            self.subject._device,
+            {POWER_DPS: True, HVACMODE_DPS: "cool"},
+        ):
+            await self.subject.async_set_hvac_mode(HVACMode.COOL)
+
+    async def test_set_hvac_mode_to_heat(self):
+        async with assert_device_properties_set(
+            self.subject._device,
+            {POWER_DPS: True, HVACMODE_DPS: "warm"},
         ):
             await self.subject.async_set_hvac_mode(HVACMode.HEAT)
 
-    async def test_turn_off(self):
+    async def test_set_hvac_mode_to_auto(self):
         async with assert_device_properties_set(
-            self.subject._device, {HVACMODE_DPS: False}
+            self.subject._device,
+            {POWER_DPS: True, HVACMODE_DPS: "smart"},
         ):
-            await self.subject.async_set_hvac_mode(HVACMode.OFF)
+            await self.subject.async_set_hvac_mode(HVACMode.HEAT_COOL)
 
     def test_preset_mode(self):
         self.dps[PRESET_DPS] = "silence"
@@ -119,11 +116,11 @@ class TestIpsProHeatpump(
         self.dps[PRESET_DPS] = "smart"
         self.assertEqual(self.subject.preset_mode, "smart")
 
-        self.dps[PRESET_DPS] = "turbo"
-        self.assertEqual(self.subject.preset_mode, "turbo")
+        self.dps[PRESET_DPS] = "booster"
+        self.assertEqual(self.subject.preset_mode, "booster")
 
     def test_preset_modes(self):
-        self.assertCountEqual(self.subject.preset_modes, ["silence", "smart", "turbo"])
+        self.assertCountEqual(self.subject.preset_modes, ["silence", "smart", "booster"])
 
     async def test_set_preset_mode_to_silent(self):
         async with assert_device_properties_set(
@@ -142,18 +139,14 @@ class TestIpsProHeatpump(
     async def test_set_preset_mode_to_smart(self):
         async with assert_device_properties_set(
             self.subject._device,
-            {PRESET_DPS: "turbo"},
+            {PRESET_DPS: "booster"},
         ):
-            await self.subject.async_set_preset_mode("turbo")
+            await self.subject.async_set_preset_mode("booster")
 
-    def test_hvac_action(self):
-        self.dps[HVACMODE_DPS] = True
-        self.dps[OPMODE_DPS] = "heating"
-        self.assertEqual(self.subject.hvac_action, HVACAction.HEATING)
-        self.dps[OPMODE_DPS] = "warm"
-        self.assertEqual(self.subject.hvac_action, HVACAction.IDLE)
-        self.dps[HVACMODE_DPS] = False
-        self.assertEqual(self.subject.hvac_action, HVACAction.OFF)
+    # Dummy test to allow model without fix range
+    async def test_set_target_temperature_fails_outside_valid_range(self):
+        self.dps[TEMPERATURE_DPS] = MAX_TEMPERATUR
+        self.assertEqual(self.targetTemp.target_temperature, MAX_TEMPERATUR)
 
     def test_extra_state_attributes(self):
         self.dps[UNKNOWN115_DPS] = 3
