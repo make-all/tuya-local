5e45b31358b534b9d361f4bb2a786fe2be72dca6
Nicolas <n.hondeghem@override.lan>
2023-04-26 20:39:51 +0200

    Add Gateway subdevices integration.

--- custom_components/tuya_local/const.py
+++ custom_components/tuya_local/const.py
@@ -6,5 +6,6 @@ CONF_DEVICE_ID = "device_id"
 CONF_LOCAL_KEY = "local_key"
 CONF_TYPE = "type"
 CONF_POLL_ONLY = "poll_only"
+CONF_DEVICE_CID = "device_cid"
 CONF_PROTOCOL_VERSION = "protocol_version"
 API_PROTOCOL_VERSIONS = [3.3, 3.1, 3.2, 3.4, 3.5]
