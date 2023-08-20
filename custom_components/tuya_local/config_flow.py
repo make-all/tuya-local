6cee1f9305fc3e7461e15e5186a30b993d187889
Jason Rumney <jasonrumney@gmail.com>
2023-08-20 22:00:21 +0900

    Add connection logging around pause/resume
    For easier debugging of issues like #968

--- custom_components/tuya_local/config_flow.py
+++ custom_components/tuya_local/config_flow.py
@@ -194,6 +194,7 @@ async def async_test_connection(config: dict, hass: HomeAssistant):
     domain_data = hass.data.get(DOMAIN)
     existing = domain_data.get(get_device_id(config)) if domain_data else None
     if existing:
+        _LOGGER.info("Pausing existing device to test new connection parameters")
         existing["device"].pause()
         await asyncio.sleep(5)
 
@@ -216,6 +217,7 @@ async def async_test_connection(config: dict, hass: HomeAssistant):
         retval = None
 
     if existing:
+        _LOGGER.info("Restarting device after test")
         existing["device"].resume()
 
     return retval
