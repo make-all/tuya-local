dc63c806451a4a4b5a2bd6f5e161d8dd9802c38f
Jason Rumney <jasonrumney@gmail.com>
2024-05-28 20:42:20 +0900

    Deprecation: log the warning when the entity is added to hass
    Logging the warning in async_setup_platform results in it being
    always logged, as we have not been through the entity registry at
    that point.
    
    Only log when the entity is actually added to hass, to avoid logging
    a warning for disabled entities.
    
    Discussion #1961

--- custom_components/tuya_local/helpers/mixin.py
+++ custom_components/tuya_local/helpers/mixin.py
@@ -106,6 +106,8 @@ class TuyaLocalEntity:
 
     async def async_added_to_hass(self):
         self._device.register_entity(self)
+        if self._config.deprecated:
+            _LOGGER.warning(self._config.deprecation_message)
 
     async def async_will_remove_from_hass(self):
         await self._device.async_unregister_entity(self)
