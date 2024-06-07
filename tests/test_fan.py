d6f0c5c31076611f7f74ac1a390a13f1cfbd407f
Aravind Jaimon <aravindjaimon.dev@gmail.com>
2024-06-07 23:11:40 +0530

    refactor(fan): removed testcases for gorilla

--- tests/test_fan.py
+++ tests/test_fan.py
@@ -120,25 +120,3 @@ async def test_init_entry_fails_if_config_is_missing(hass):
     except ValueError:
         pass
     m_add_entities.assert_not_called()
-
-@pytest.mark.asyncio
-async def test_init_entry_for_gorilla_fan_v2(hass):
-    """Test initialisation for Gorilla Fan V2"""
-    entry = MockConfigEntry(
-        domain=DOMAIN,
-        data={
-            CONF_TYPE: "gorilla_fan_v2",
-            CONF_DEVICE_ID: "dummy",
-            CONF_PROTOCOL_VERSION: "auto",
-        },
-    )
-    m_add_entities = Mock()
-    m_device = AsyncMock()
-
-    hass.data[DOMAIN] = {}
-    hass.data[DOMAIN]["dummy"] = {}
-    hass.data[DOMAIN]["dummy"]["device"] = m_device
-
-    await async_setup_entry(hass, entry, m_add_entities)
-    assert type(hass.data[DOMAIN]["dummy"]["fan"]) == TuyaLocalFan
-    m_add_entities.assert_called_once()
