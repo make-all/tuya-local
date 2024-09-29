8e1ff4b03e5a1734cbd05e19e3a5ea76bd9f6f09
Jason Rumney <jasonrumney@gmail.com>
2024-09-29 17:12:36 +0900

    Duux Blizzard tests: clean up unused imports

--- tests/devices/test_duux_blizzard.py
+++ tests/devices/test_duux_blizzard.py
@@ -1,8 +1,6 @@
-from homeassistant.components.climate.const import ClimateEntityFeature, HVACMode
-from homeassistant.const import UnitOfTemperature
+from homeassistant.components.climate.const import HVACMode
 
 from ..const import DUUX_BLIZZARD_PAYLOAD
-from ..helpers import assert_device_properties_set
 from ..mixins.climate import TargetTemperatureTests
 from .base_device_tests import TuyaDeviceTestCase
 
