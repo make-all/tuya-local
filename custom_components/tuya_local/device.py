08db14e69ff3ec75aa96af78e364ff39ea703422
Jason Rumney <jasonrumney@gmail.com>
2024-11-02 09:20:49 +0900

    Device: fix boolean case

--- custom_components/tuya_local/device.py
+++ custom_components/tuya_local/device.py
@@ -254,7 +254,7 @@ class TuyaLocalDevice(object):
 
     def pause(self):
         self._temporary_poll = True
-        self._api.setSocketPersistent(false)
+        self._api.setSocketPersistent(False)
         if self._api.parent:
             self._api.parent.setSocketPersistent(False)
 
