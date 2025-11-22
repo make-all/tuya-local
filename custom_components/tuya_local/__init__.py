0acce83204025d8eef8aec374781d026f60e7060
Jason Rumney <jasonrumney@gmail.com>
2025-11-22 18:15:36 +0900

    feat (translations): migrate "Disturb switch" to do_not_disturb
    Variation on the now translated "Do not disturb" used by 3 vacuums.
    
    Issue #1708

--- custom_components/tuya_local/__init__.py
+++ custom_components/tuya_local/__init__.py
@@ -631,6 +631,33 @@ async def async_migrate_entry(hass, entry: ConfigEntry):
 
         await async_migrate_entries(hass, entry.entry_id, update_unique_id13_9)
         hass.config_entries.async_update_entry(entry, minor_version=9)
+
+    if entry.version == 13 and entry.minor_version < 10:
+        # Migrate unique ids of existing entities to new id taking into
+        # account translation_key, and standardising naming
+        device_id = entry.unique_id
+        conf_file = await hass.async_add_executor_job(
+            get_config,
+            entry.data[CONF_TYPE],
+        )
+        if conf_file is None:
+            _LOGGER.error(
+                NOT_FOUND,
+                entry.data[CONF_TYPE],
+            )
+            return False
+
+        @callback
+        def update_unique_id13_10(entity_entry):
+            """Update the unique id of an entity entry."""
+            # Standardistion of entity naming to use translation_key
+            replacements = {
+                "switch_disturb_switch": "switch_do_not_disturb",
+            }
+            return replace_unique_ids(entity_entry, device_id, conf_file, replacements)
+
+        await async_migrate_entries(hass, entry.entry_id, update_unique_id13_10)
+        hass.config_entries.async_update_entry(entry, minor_version=10)
     return True
 
 
