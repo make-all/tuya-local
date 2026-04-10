2c91850e2494a22d6160b6454a8be8eda328eb7d
Jason Rumney <jasonrumney@gmail.com>
2026-04-11 08:43:52 +0900

    tests: fix infrared expected data after proper json formatting

--- tests/test_infrared.py
+++ tests/test_infrared.py
@@ -122,11 +122,11 @@ async def test_async_send_command(mocker):
     async with assert_device_properties_set(
         device,
         {
-            "201": (
-                "{'control': 'send_ir', 'type': 0, 'head': '', 'key1': '1KCOUETICMgIyAjI"
+            "201"(
+                '"control": "send_ir", "type": 0, "head": "", "key1": "1KCOUETICMgIyAjI'
                 "CMgKXBjICMgIyAjICMgIyAjICMgIyApcGMgIyAjIClwYyAjICMgIyAjIClwYyAjICMgKXBj"
                 "ICMgIyAjICMgKXBjICMgIyAjICMgKXBjIClwYyAjICMgIyAjIClwYyAjICMgKXBjIClwYyA"
-                "jICMgIyAjIClwYyApcGMgIAAA=='}"
+                'jICMgIyAjIClwYyApcGMgIAAA=="}'
             )
         },
     ):
