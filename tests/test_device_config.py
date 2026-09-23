92f52c97b728a6e39815dbeb707e41c6404acb30
Jason Rumney <jasonrumney@gmail.com>
2026-09-23 14:41:06 +0900

    fix(tests): allow float for condition, not mapping dps_val

--- tests/test_device_config.py
+++ tests/test_device_config.py
@@ -36,7 +36,7 @@ PRODUCT_SCHEMA = vol.Schema(
 )
 CONDMAP_SCHEMA = vol.Schema(
     {
-        vol.Optional("dps_val"): vol.Maybe(vol.Any(str, int, bool, list, float)),
+        vol.Optional("dps_val"): vol.Maybe(vol.Any(str, int, bool, list)),
         vol.Optional("value"): vol.Maybe(vol.Any(str, int, bool, float)),
         vol.Optional("value_redirect"): str,
         vol.Optional("value_mirror"): str,
@@ -62,7 +62,7 @@ CONDMAP_SCHEMA = vol.Schema(
 )
 COND_SCHEMA = CONDMAP_SCHEMA.extend(
     {
-        vol.Required("dps_val"): vol.Maybe(vol.Any(str, int, bool, list)),
+        vol.Required("dps_val"): vol.Maybe(vol.Any(str, int, bool, list, float)),
         vol.Optional("mapping"): [CONDMAP_SCHEMA],
     }
 )
